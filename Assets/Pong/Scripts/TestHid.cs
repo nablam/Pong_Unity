0e54955214770746ae6109bc0a952c221227b931
nablam <lamribenn@gmail.com>
2022-01-12 15:11:18 -0500

    axis gravity 0, dead zone 0 , sensitivity, 1, arduino maps 0-1023 to 0-127, unity converts 1 to -1" /

--- Assets/Pong/Scripts/TestHid.cs
+++ Assets/Pong/Scripts/TestHid.cs
@@ -13,7 +13,9 @@ public class TestHid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
-        Debug.Log(Input.GetAxisRaw("Vertical"));
+        // Debug.Log(Input.GetAxisRaw("Horizontal"));
+        Debug.Log(Input.GetAxis("Vertical1")+ " " + Input.GetAxis("Vertical2")) ;
+       // Debug.Log(Input.GetAxisRaw("Vertical1") + " " + Input.GetAxisRaw("Vertical2"));
+        //Debug.Log(Input.GetAxisRaw("P1_button1") + " " + Input.GetAxisRaw("P2_button1"));
     }
 }
