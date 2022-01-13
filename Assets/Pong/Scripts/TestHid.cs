4f15d2dea7b500ef6d2e25bd2a9e9e564942ae38
nablam <lamribenn@gmail.com>
2022-01-13 11:41:40 -0500

    was working with problms. a ange of 0-127 in aduino converts to -1 to 1 in unity , wbut then 0-to 127 on raspbery pi . this is an HID issue. I tried using the joystick2.0 lib but now no inoput is registered. also joystick2 lib (the multijoy) had indexing issues accross platforms, joystick 2 was not reading in pi

--- Assets/Pong/Scripts/TestHid.cs
+++ Assets/Pong/Scripts/TestHid.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class TestHid : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject Oj1;
+    public GameObject Oj2;
+    public GameObject Ojbutt;
+
+    TextMeshPro J1;
+    TextMeshPro J2;
+    TextMeshPro butttons;
+    string tmp = "";
     void Start()
     {
-        
+        J1 = Oj1.GetComponent<TextMeshPro>();
+        J2 = Oj2.GetComponent<TextMeshPro>();
+        butttons = Ojbutt.GetComponent<TextMeshPro>();
+
+
     }
 
     // Update is called once per frame
@@ -17,5 +32,12 @@ public class TestHid : MonoBehaviour
         // Debug.Log(Input.GetAxis("Vertical1")+ " " + Input.GetAxis("Vertical2")) ;
         // Debug.Log(Input.GetAxisRaw("Vertical1") + " " + Input.GetAxisRaw("Vertical2"));
         Debug.Log(Input.GetAxisRaw("P1_button1") + " " + Input.GetAxisRaw("P2_button1"));
+        J1.text = Input.GetAxis("Vertical1").ToString();
+        J2.text = Input.GetAxisRaw("Vertical2").ToString();
+
+  
+
+        tmp = Input.GetAxisRaw("P1_button1").ToString() + " " + Input.GetAxisRaw("P2_button1").ToString();
+        butttons.text = tmp;
     }
 }
